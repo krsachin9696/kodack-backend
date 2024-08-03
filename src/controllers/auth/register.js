3bb1273ea6ed5bfb5769c5dcf4085d3938536ceb
krsachin9696 <krsachin9696@gmail.com>
2024-08-03 18:17:52 +0530

    update

--- src/controllers/auth/register.js
+++ src/controllers/auth/register.js
@@ -7,10 +7,29 @@ const register = async (req, res) => {
   const { name, username, email, password } = req.body;
 
   try {
+    // Check if the username or email already exists
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        OR: [
+          { username },
+          { email },
+        ],
+      },
+    });
+
+    if (existingUser) {
+      return res.status(400).send('Username or email already in use');
+    }
+
+    // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create the new user
     await prisma.user.create({
       data: { name, username, email, passwordHash: hashedPassword },
     });
+
+    // Redirect to the login page or send a success message
     res.redirect('/login');
   } catch (error) {
     console.log(error);
