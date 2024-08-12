cdeaf05b68e3568ea51ca11e10c203db1c02ae63
krsachin9696 <krsachin9696@gmail.com>
2024-08-12 17:58:10 +0530

    implemented session based authentication

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: true },
+    // cookie: { secure: true }, // making this true ensures setting cookies in HTTPS only.
   }),
 );
 
