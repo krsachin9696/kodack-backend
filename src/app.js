cc968d7c640f094d4a1076deb9aa7bbc5afc42f4
krsachin9696 <krsachin9696@gmail.com>
2024-09-06 10:32:52 +0530

    session-store db

--- src/app.js
+++ src/app.js
@@ -3,9 +3,12 @@ import 'dotenv/config';
 import cors from 'cors';
 import session from 'express-session';
 import passport from 'passport';
+import connectPgSimple from 'connect-pg-simple';
 import './config/passport.js';
 import { authRoute, listRoute, questionsRoute } from './routes/index.js';
 import isAuthenticated from './middlewares/authMiddleware.js';
+
+const PgSession = connectPgSimple(session);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -26,11 +29,17 @@ app.use(
 // Configure session middleware
 app.use(
   session({
+    store: new PgSession({
+      conString: process.env.DATABASE_URL,
+      createTableIfMissing: true,
+    }),
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    // cookie: { secure: true }, // making this true ensures setting cookies in HTTPS only.
-    maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+      secure: false, // making this true ensures setting cookies in HTTPS only.
+    },
   }),
 );
 
